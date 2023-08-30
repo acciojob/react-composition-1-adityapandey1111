70a9904ef300f22a0e4b84bd35a9d50570f6af84
Aditya Kumar Pandey <114282369+adityapandey1111@users.noreply.github.com>
2023-08-30 16:35:47 +0000

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Tabs from './Tabs';
 
 const tabData = [
-  { title: 'Tab 1', content: 'This is the ontent for Tab 1' },
-  { title: 'Tab 2', content: 'This is the ontent for Tab 2' },
-  { title: 'Tab 3', content: 'This is the ontent for Tab 3' },
+  { title: 'Tab 1', content: 'This is the content for Tab 1' },
+  { title: 'Tab 2', content: 'This is the content for Tab 2' },
+  { title: 'Tab 3', content: 'This is the content for Tab 3' },
 ];
 
 const App = () => {
